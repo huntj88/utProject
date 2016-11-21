856955c17c574872e9abf9b12a8dc263e0205ee1
Choi , Jin W <jwc2728@lib-pclem029.lib.utexas.edu>
2016-11-21 16:55:25 -0600

    Hell yeah

--- Final Project/ViewController.swift
+++ Final Project/ViewController.swift
@@ -20,6 +20,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func login(sender: AnyObject) {
+        performSegueWithIdentifier("loginSeg", sender: nil)
+        print ("let's gooo!")
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!){
+        if segue.identifier == "loginSeg" {
+            print("gen")
+            if let nextView: initialItemScreenCollectionViewController = segue.destinationViewController as? initialItemScreenCollectionViewController{
+                print("correct VC, second screen")
+                //nextVC.incomingText = self.input.text!
+                
+            }
+        }
+        if segue.identifier == "toItem"{
+            print("toItem")
+            if let nextView: initialItemScreenCollectionViewController = segue.destinationViewController as? initialItemScreenCollectionViewController{
+                print("correct VC, second screen")
+        }
+    }
 }
 
