3a7dafb313be4413311d4ef22a71993545fd3cba
Daniel Choi <daniel5815@gmail.com>
2016-11-21 17:53:39 -0600

    login started

--- Final Project/ViewController.swift
+++ Final Project/ViewController.swift
@@ -21,9 +21,46 @@ class ViewController: UIViewController {
     }
 
     @IBAction func login(sender: AnyObject) {
-        performSegueWithIdentifier("loginSeg", sender: nil)
-        print ("let's gooo!")
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: "http://138.68.41.247:2996/users/login")!)
+        request.HTTPMethod = "POST"
+        let postString = "email=huntj88@gmail.com&password=test"
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
+            guard error == nil && data != nil else {                                                          // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? NSHTTPURLResponse where httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+        
+            
+            let responseString = String(data: data!, encoding: NSUTF8StringEncoding)!
+            print("responseString = \(responseString)")
+            
+            let json: AnyObject? = responseString.parseJSONString
+            
+            if let item = json![0] as? [String: AnyObject] {
+                if let email = item["email"] as? String {
+                    print(email)
+                    self.performSegueWithIdentifier("loginSeg", sender: nil)
+                    print ("let's gooo!")
+                }
+            }
+            
+            
+        }
+        task.resume()
+        
+        
     }
+    
+    
+    
+
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!){
         if segue.identifier == "loginSeg" {
@@ -41,4 +78,4 @@ class ViewController: UIViewController {
         }
     }
 }
-
+}
