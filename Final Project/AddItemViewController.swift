bf10244e0787abc0ffc488f5987b48911c8a88b9
Hunt , James V <jvh445@lib-pclem020.lib.utexas.edu>
2016-11-30 01:59:18 -0600

    images

--- Final Project/AddItemViewController.swift
+++ Final Project/AddItemViewController.swift
@@ -76,16 +76,23 @@ class AddItemViewController: UIViewController, UICollectionViewDataSource, UICol
             
             print(responseString)
             
-            if(responseString=="success")
+            for x in 0..<self.selectedImagesArray.count
             {
+                self.myImageUploadRequest(x,itemID: responseString)
+            }
+            
+            
+            
+            //if(responseString=="success")
+            //{
                 print(self.delegate)
                 self.delegate?.setRefresh()
                 dispatch_async(dispatch_get_main_queue()) {
                     self.navigationController?.popToRootViewControllerAnimated(true)
                 }
 
-                
-            }
+            
+            //}
             
             
             
@@ -166,5 +173,113 @@ class AddItemViewController: UIViewController, UICollectionViewDataSource, UICol
         cell.itemImage.image = selectedImagesArray[indexPath.row]
         return cell
         
+    }
+    
+    
+    func myImageUploadRequest(imageIndex: Int,itemID: String)
+    {
+        
+        let myUrl = NSURL(string: "http://138.68.41.247:2996/items/uploadImage");
+        //let myUrl = NSURL(string: "http://www.boredwear.com/utils/postImage.php");
+        
+        let request = NSMutableURLRequest(URL:myUrl!);
+        request.HTTPMethod = "POST";
+        
+        let param = [
+            "userID"  : String(userID!),
+            "apiKey"    : apiKey!,
+            "itemID":itemID
+        ]
+        
+        let boundary = generateBoundaryString()
+        
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        
+        
+        let imageData = UIImageJPEGRepresentation(selectedImagesArray[imageIndex], 1)
+        
+        if(imageData==nil)  { return; }
+        
+        request.HTTPBody = createBodyWithParameters(param, filePathKey: "file", imageDataKey: imageData!, boundary: boundary)
+        
+        
+        //myActivityIndicator.startAnimating();
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            if error != nil {
+                print("error=\(error)")
+                return
+            }
+            
+            // You can print out response object
+            print("******* response = \(response)")
+            
+            // Print out reponse body
+            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print("****** response data = \(responseString!)")
+            
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary
+                
+                print(json)
+                
+                dispatch_async(dispatch_get_main_queue(),{
+                    //self.myActivityIndicator.stopAnimating()
+                    //self.myImageView.image = nil;
+                });
+                
+            }catch
+            {
+                print(error)
+            }
+            
+        }
+        
+        task.resume()
+    }
+    
+    
+    func createBodyWithParameters(parameters: [String: String]?, filePathKey: String?, imageDataKey: NSData, boundary: String) -> NSData {
+        let body = NSMutableData();
+        
+        if parameters != nil {
+            for (key, value) in parameters! {
+                body.appendString("--\(boundary)\r\n")
+                body.appendString("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n")
+                body.appendString("\(value)\r\n")
+            }
+        }
+        
+        let filename = "user-profile.jpg"
+        let mimetype = "image/jpg"
+        
+        body.appendString("--\(boundary)\r\n")
+        body.appendString("Content-Disposition: form-data; name=\"\(filePathKey!)\"; filename=\"\(filename)\"\r\n")
+        body.appendString("Content-Type: \(mimetype)\r\n\r\n")
+        body.appendData(imageDataKey)
+        body.appendString("\r\n")
+        
+        
+        
+        body.appendString("--\(boundary)--\r\n")
+        
+        return body
+    }
+    
+    
+    
+    func generateBoundaryString() -> String {
+        return "Boundary-\(NSUUID().UUIDString)"
+    }
+    
+}
+
+extension NSMutableData {
+    
+    func appendString(string: String) {
+        let data = string.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
+        appendData(data!)
     }
 }
