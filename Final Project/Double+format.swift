0f41540c2647980f6b6d021696258f7a336f01f0
Hunt , James V <jvh445@lib-pclem044.lib.utexas.edu>
2016-12-04 20:48:38 -0600

    money 3

--- Final Project/Double+format.swift
+++ Final Project/Double+format.swift
@@ -8,7 +8,13 @@
 
 import Foundation
 extension Double {
-    func money() -> String {
+    /*func money() -> String {
         return "$"+String(format: "%\(".0")f", self)
+    }*/
+    
+    func money() -> String{
+        let numberFormatter = NSNumberFormatter()
+        numberFormatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
+        return "$" + numberFormatter.stringFromNumber(self)!
     }
 }
